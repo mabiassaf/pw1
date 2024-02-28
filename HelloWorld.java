fd931f39f855bcd20b2382c3c422a0e6c96d473a
mabiassaf <112391349+mabiassaf@users.noreply.github.com>
2024-02-28 11:08:54 +0200

    Fifth Commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -10,5 +10,13 @@ class HelloWorld {
 		System.out.println("How are you?"); 
 		System.out.println("I am fine"); 
 		System.out.println("How old are you?"); 
+        System.out.println("Hello, World!"); 
+		System.out.println("How are you?"); 
+		System.out.println("I am fine"); 
+		System.out.println("How old are you?"); 
+        System.out.println("Hello, World!"); 
+		System.out.println("How are you?"); 
+		System.out.println("I am fine"); 
+		System.out.println("How old are you?"); 
     }
 }
